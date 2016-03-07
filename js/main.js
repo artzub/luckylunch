979007f8eca6a191d3e9f014d6cd4db8679b52c0
Brian Herhusky <herhusky@gmail.com>
2016-03-07 13:56:58 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -831,7 +831,7 @@ var settings = {
                         if (!parentMarker[tp._id] && d.parentNode.nodeValue instanceof Supplier) {
                             var marker = parentMarker[tp._id] = L.marker(tp.latlng, {
                                 icon: L.mapbox.marker.icon({
-                                    'marker-symbol': tp instanceof Supplier ? 'restaurant' : 'warehouse' ,
+                                    'marker-symbol': tp instanceof Supplier ? 'Hotel' : 'Agency' ,
                                     'marker-color': '222234'
                                 })
                             });
